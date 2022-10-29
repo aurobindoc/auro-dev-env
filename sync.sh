da47ae394963776d49aeca1f175006c54aa97094
aurobindoc <aurobindoc1991@gmail.com>
2022-10-30 01:26:20 +0530

    updated sync.sh

--- sync.sh
+++ sync.sh
@@ -5,7 +5,7 @@ cp /Users/aurobindo.m/.zshrc .zshrc
 cp /Users/aurobindo.m/.p10k.zsh .p10k.zsh
 
 # Copy nvim files
-cp -r /Users/aurobindo.m/.config/nvim nvim
+cp -r /Users/aurobindo.m/.config/nvim . 
 
 # Copy color scheme
 cp /Users/aurobindo.m/tools/iterm/auro.itermcolors .
